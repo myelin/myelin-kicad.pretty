52a0c2a8c5a355e851128aa5ab768d7728726417
Phillip Pearson <philpearson@google.com>
2019-05-20 23:19:06 -0700

    Added via array to help grounding around a Lattice FTG256 FPGA

--- via_arrays.py
+++ via_arrays.py
@@ -3,7 +3,7 @@ from myelin_kicad_mod import *
 VIA_PAD = 0.032 * INCH
 VIA_HOLE = 0.013 * INCH
 
-def make_straight_module(N, SPACING, SPACING_STR=None):
+def make_straight_module(N, SPACING, SPACING_STR=None, pad=VIA_PAD, hole=VIA_HOLE):
 	X = Module(
 		identifier="via_single" if N == 1 else "via_array_1x%d_%s" % (N, SPACING_STR),
 		description="Via array for stapling planes together",
@@ -14,8 +14,8 @@ def make_straight_module(N, SPACING, SPACING_STR=None):
 			name=i+1,
 			x=0 if N==1 else (i - float(N)/2) * SPACING,
 			y=0,
-			dia=VIA_PAD,
-			hole_dia=VIA_HOLE,
+			dia=pad,
+			hole_dia=hole,
 			solid_connect=True,
 			layers=["*.Cu"],
 		))
@@ -42,4 +42,5 @@ def make_zigzag_module(N, SPACING, SPACING_STR=None):
 make_straight_module(1, 2.54, "2.54mm")
 make_straight_module(7, 0.055 * INCH, "55mil")
 make_straight_module(22, 2.54, "2.54mm")
+make_straight_module(51, 1.0, "1mm", pad=0.6, hole=0.3)
 make_zigzag_module(44, 1.27, "1.27mm")
